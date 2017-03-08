2334cac64fdff5ab9f5c1cd6db9759a5fa50bb83
Joel Wallace <jwalks@ksu.edu>
2017-03-08 14:56:43 -0600

    added cover app

--- coverage/views.py
+++ coverage/views.py
@@ -11,16 +11,16 @@ def cover(request, staff, command):
         for st in staff_members:
             url = "http://{}.oeie.org:5138/blink1/on".format(st.staff)
             r = requests.get(url)
-            if r.status_code == 200:
-                messages.success(request, "{}'s light was notified.".format(st.staff))
-            else:
-                messages.error(request, "{}'s light was NOT notified.".format(st.staff))
+            #if r.status_code == 200:
+            #    messages.success(request, "{}'s light was notified.".format(st.staff))
+            #else:
+            #    messages.error(request, "{}'s light was NOT notified.".format(st.staff))
     elif command == "cover":
         color = Color.objects.get(staff=staff)
         for st in staff_members:
             url = "http://{}.oeie.org:5138/blink1/fadeToRGB?rgb=%23".format(st.staff, color.color)
             r = requests.get(url)
-            if r.status_code == 200:
-                messages.success(request, "{}'s light was notified.".format(st.staff))
-            else:
-                messages.error(request, "{}'s light was NOT notified.".format(st.staff))
+            #if r.status_code == 200:
+            #    messages.success(request, "{}'s light was notified.".format(st.staff))
+            #else:
+            #    messages.error(request, "{}'s light was NOT notified.".format(st.staff))
