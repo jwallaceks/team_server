1904ea8c6fd432a63164b5b373b9f5d57b30d59f
Joel Wallace <jwalks@ksu.edu>
2017-03-08 15:18:09 -0600

    added cover app

--- coverage/views.py
+++ coverage/views.py
@@ -1,6 +1,6 @@
 from django.views import generic
 from django.contrib import messages
-import unirest
+import requests
 from django.http import HttpResponseRedirect
 
 from .models import Color
@@ -18,7 +18,8 @@ def cover(request, staff, command):
     if command == "release":
         for st in staff_members:
             url = "http://{}.oeie.org:5138/blink1/on".format(st.staff)
-            thread = unirest.get(url, callback=callback_function)
+            print(url)
+            r = request.get(url)
             #if r.status_code == 200:
             #    messages.success(request, "{}'s light was notified.".format(st.staff))
             #else:
@@ -27,9 +28,11 @@ def cover(request, staff, command):
         color = Color.objects.get(staff=staff)
         for st in staff_members:
             url = "http://{}.oeie.org:5138/blink1/fadeToRGB?rgb=%23".format(st.staff, color.color)
-            thread = unirest.get(url, callback=callback_function)
+            print(url)
+            r = requests.get(url)
             #if r.status_code == 200:
             #    messages.success(request, "{}'s light was notified.".format(st.staff))
             #else:
             #    messages.error(request, "{}'s light was NOT notified.".format(st.staff))
     return HttpResponseRedirect('/')
+
