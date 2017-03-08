f527ebf6ff6fcd7f768e8e45f2d887aea4e08929
Joel Wallace <jwalks@ksu.edu>
2017-03-08 15:04:14 -0600

    added cover app

--- coverage/views.py
+++ coverage/views.py
@@ -1,16 +1,18 @@
 from django.views import generic
 from django.contrib import messages
-import requests
+import grequests
 
 from .models import Color
 
 
 def cover(request, staff, command):
     staff_members = Color.objects.all()
+    urls = []
     if command == "release":
         for st in staff_members:
             url = "http://{}.oeie.org:5138/blink1/on".format(st.staff)
-            r = requests.get(url)
+            urls +=[url,]
+#            r = requests.get(url)
             #if r.status_code == 200:
             #    messages.success(request, "{}'s light was notified.".format(st.staff))
             #else:
@@ -19,8 +21,11 @@ def cover(request, staff, command):
         color = Color.objects.get(staff=staff)
         for st in staff_members:
             url = "http://{}.oeie.org:5138/blink1/fadeToRGB?rgb=%23".format(st.staff, color.color)
-            r = requests.get(url)
+            urls += [url, ]
+            #r = requests.get(url, timeout=0.001)
             #if r.status_code == 200:
             #    messages.success(request, "{}'s light was notified.".format(st.staff))
             #else:
             #    messages.error(request, "{}'s light was NOT notified.".format(st.staff))
+    rs = (grequests.get(u) for u in urls)
+    grequests.map(rs)
