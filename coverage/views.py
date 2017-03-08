c1cc04c7784ecaaa3b884f4f836ffeab385c6c8c
Joel Wallace <jwalks@ksu.edu>
2017-03-08 14:55:13 -0600

    added cover app

--- coverage/views.py
+++ coverage/views.py
@@ -9,7 +9,7 @@ def cover(request, staff, command):
     staff_members = Color.objects.all()
     if command == "release":
         for st in staff_members:
-            url = "{}.oeie.org:5138/blink1/on".format(st.staff)
+            url = "http://{}.oeie.org:5138/blink1/on".format(st.staff)
             r = requests.get(url)
             if r.status_code == 200:
                 messages.success(request, "{}'s light was notified.".format(st.staff))
@@ -18,7 +18,7 @@ def cover(request, staff, command):
     elif command == "cover":
         color = Color.objects.get(staff=staff)
         for st in staff_members:
-            url = "{}.oeie.org:5138/blink1/fadeToRGB?rgb=%23".format(st.staff, color.color)
+            url = "http://{}.oeie.org:5138/blink1/fadeToRGB?rgb=%23".format(st.staff, color.color)
             r = requests.get(url)
             if r.status_code == 200:
                 messages.success(request, "{}'s light was notified.".format(st.staff))
