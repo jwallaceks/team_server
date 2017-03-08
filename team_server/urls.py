ce74587c8d865df53c27645890292ccd4f789708
Joel Wallace <jwalks@ksu.edu>
2017-03-08 14:52:32 -0600

    added cover app

--- team_server/urls.py
+++ team_server/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
+from coverage.views import cover
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^grappelli/', include('grappelli.urls')),
+    url(r'^(?P<staff>[a-z]+)/(?P<command>[a-z]+)/$', cover, name='cover'),
 ]
