fb5cec8b6e08fa46d5475de0f563fa4314e434db
Joel Wallace <jwalks@ksu.edu>
2017-03-09 09:26:33 -0600

    Moved to bash form

--- coverage/views.py
+++ coverage/views.py
@@ -2,8 +2,10 @@ from django.views import generic
 from django.contrib import messages
 from .models import Color
 import subprocess
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+import json
+
 
 @csrf_exempt
 def cover(request):
@@ -17,13 +19,17 @@ def cover(request):
         rc = subprocess.call("/srv/sites/team_server/coverage/jessie.sh")
     if staff=="zac":
         rc = subprocess.call("/srv/sites/team_server/coverage/zac.sh")
-    return HttpResponse("Coverage updated. {} is now covering the phone.".format(staff))
+    return JsonResponse({"response_type": "in_channel","text": "Coverage updated. {} is now covering the phone.".format(staff)})
+
+
 
 @csrf_exempt
 def release(request):
     staff = request.POST['user_name']
     rc = subprocess.call("/srv/sites/team_server/coverage/release.sh")
-    return HttpResponse("{} released coverage. Someone please cover the phone.".format(staff))
+    return JsonResponse(
+        {"response_type": "in_channel", "text": "{} released coverage. Someone please cover the phone.".format(staff)})
+
 
 def index(request):
     return HttpResponse("Run a different command to update coverage.")
