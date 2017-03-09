54f1a4fc895b2d9b09db5e56939629dc13dd81ef
Joel Wallace <jwalks@ksu.edu>
2017-03-09 08:40:16 -0600

    Moved to bash form

--- coverage/joel.sh
+++ coverage/joel.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-nohup curl http://joel.oeie.org:5138/blink1/fadeToRGB?rgb=%239700BD &
-nohup curl http://trent.oeie.org:5138/blink1/fadeToRGB?rgb=%239700BD &
-nohup curl http://jessie.oeie.org:5138/blink1/fadeToRGB?rgb=%239700BD &
-nohup curl http://zac.oeie.org:5138/blink1/fadeToRGB?rgb=%239700BD &
+nohup curl http://joel.oeie.org:5138/blink1/fadeToRGB?rgb=%2301563e &
+nohup curl http://trent.oeie.org:5138/blink1/fadeToRGB?rgb=%2301563e &
+nohup curl http://jessie.oeie.org:5138/blink1/fadeToRGB?rgb=%2301563e &
+nohup curl http://zac.oeie.org:5138/blink1/fadeToRGB?rgb=%2301563e &
